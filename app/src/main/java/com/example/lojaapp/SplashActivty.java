2b81c59c91c5ae00c8032d77ef3f9bed225c1c1f
GabrielAlbSilv <gabrielalbuquerquemoura@gmai.com>
2021-09-08 20:20:15 -0300

    cores e manifest

---
+++ app/src/main/java/com/example/lojaapp/SplashActivty.java
@@ -0,0 +1,14 @@
+package com.example.lojaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activty_layout);
+    }
+}
